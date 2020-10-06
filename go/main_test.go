48256363177eacff6bbecb62034682ee8fc44a94
John Puggian <puggianjv@gmail.com>
2020-10-06 10:12:15 -0300

    Adding dot to greeting

--- go/main_test.go
+++ go/main_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func TestGreeting(t *testing.T) {
 	result := greeting("hello")
-	expected := "<b>hello</b>"
-    if result != expected {
-        t.Errorf("greeting('hello') %s; want '<b>hello</b>'", result)
-    }
+	expected := "<b>hello</b>."
+	if result != expected {
+		t.Errorf("greeting('hello') %s; want '<b>hello</b>.'", result)
+	}
 }
